042ca4fed303a926287d48418e9f00080410f64a
Sara <77059785+alltheakhlaq@users.noreply.github.com>
2026-01-05 12:32:40 +0000

    Minor style changes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Counterdata Visualisation of Digital Museum Collections" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
