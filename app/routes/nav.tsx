d76de50981bac38adce060e799b953320d9390f9
Sara <77059785+alltheakhlaq@users.noreply.github.com>
2026-01-06 11:36:36 +0000

    added nav

--- app/routes/nav.tsx
+++ app/routes/nav.tsx
@@ -1,15 +1,25 @@
+import { Link } from "react-router";
+
 const navItems = [
-  { title: "Home" },
-  { title: "Documentation" },
-  { title: "Vocabulary" },
-  { title: "Literature" },
+  { title: "Home", to: "/" },
+  { title: "Documentation", to: "/documentation" },
+  { title: "Vocabulary", to: "/Vocabulary" },
+  { title: "Literature", to: "/Literature" },
 ];
 
 export function Nav() {
   return (
     <nav className="flex flex-row">
       {navItems.map((item) => (
-        <div className="border-solid border-2">{item.title}</div>
+        <div className="border-solid border-2">
+          {/* {item.title} */}
+          <Link
+            className="group flex items-center gap-3 self-stretch pt-3 leading-normal underline hover:decoration-amber-300"
+            to={item.to}
+          >
+            {item.title}
+          </Link>
+        </div>
       ))}
     </nav>
   );
